fc79a0d588acebe6896ab107a6838100012c41d9
Mark Wong <mark.wong@myemma.com>
2011-10-25 11:35:21 -0500

    add config_set_i back to postgresql plugin
    It was removed in a04ffbda508739433df0975328100e33e7986c87 but I need it
    for annotating the metal data with database, schema, table and index
    names.

--- src/postgresql.c
+++ src/postgresql.c
@@ -792,6 +792,28 @@ static int config_set_s (char *name, char **var, const oconfig_item_t *ci)
 	return 0;
 } /* config_set_s */
 
+static int config_set_i (char *name, int *var,
+		const oconfig_item_t *ci, int min)
+{
+	int value;
+
+	if ((0 != ci->children_num) || (1 != ci->values_num)
+			|| (OCONFIG_TYPE_NUMBER != ci->values[0].type)) {
+		log_err ("%s expects a single number argument.", name);
+		return 1;
+	}
+
+	value = (int)ci->values[0].value.number;
+
+	if (value < min) {
+		log_err ("%s expects a number greater or equal to %i.", name, min);
+		return 1;
+	}
+
+	*var = value;
+	return 0;
+} /* config_set_i */
+
 static int config_query_param_add (udb_query_t *q, oconfig_item_t *ci)
 {
 	c_psql_user_data_t *data;
